df233f2c18a0db36d710f50f64aa98b069f2d29d
juillianlee <juillianlee@hotmail.com>
2019-05-17 15:02:43 -0300

    Ajustes

---
+++ index.js
@@ -0,0 +1,93 @@
+const program = require('commander');
+const inquirer = require('inquirer');
+const package = require('./package.json');
+const fs = require('fs');
+const chalk = require('chalk');
+
+program.version(package.version);
+
+
+const FILES = [
+    {
+        template: 'templates/template-store.module.template',
+        file: '##STATE_NAME##-store.module.ts'
+    },
+    {
+        template: 'templates/template-store.service.template',
+        file: '##STATE_NAME##-store.service.ts'
+    },
+    {
+        template: 'templates/template.actions.template',
+        file: '##STATE_NAME##.actions.ts'
+    },
+    {
+        template: 'templates/template.service.template',
+        file: '##STATE_NAME##.service.ts'
+    },
+    {
+        template: 'templates/template.effects.template',
+        file: '##STATE_NAME##.effects.ts'
+    },
+    {
+        template: 'templates/template.reducer.template',
+        file: '##STATE_NAME##.reducer.ts'
+    },
+    {
+        template: 'templates/template.state.template',
+        file: '##STATE_NAME##.state.ts'
+    }
+];
+
+
+
+const prompt = [
+    {
+        type: 'input',
+        name: 'store',
+        message: 'Qual o nome da sua store',
+        validate: value => value ? true : 'Não é permitido vázio'
+    }
+]
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function replaceNames(storeName, string) {
+    return string.replace(/##STATE_NAME_CLASS##/g, capitalizeFirstLetter(storeName))
+    .replace(/##STATE_NAME##/g, storeName.toLowerCase())
+    .replace(/##STATE_NAME_ACTIONS##/g, storeName.toUpperCase());
+}
+
+async function createFile(storeName, f) {
+    const fileName = f.file.replace('##STATE_NAME##', storeName.toLowerCase());
+    console.log(`${chalk.magenta('Criando o arquivo: ' + fileName +  ' com base no Template: ' + f.template)}`);
+    const content = replaceNames(storeName, fs.readFileSync(f.template, 'utf8'));
+    if(!fs.existsSync(`${storeName}/${fileName}`)) {
+        await fs.writeFileSync(`${storeName}/${fileName}`, content);
+        console.log(`${chalk.magenta('Arquivo : ' + fileName +  ' Gerado com sucesso')}`);
+    } else {
+        console.log(`${chalk.red('Arquivo : ' + fileName +  ' já existe....')}`);
+    }
+    
+}
+
+program
+    .command('generate')
+    .description('Criar a estrutura de redux de uma store em angular')
+    .action(async (_) => {
+        perguntas = await inquirer.prompt(prompt);
+        console.log(`${chalk.green('Gerando os arquivos com o nome: ' + perguntas.store)}`);
+        const storeName = perguntas.store.toLowerCase();
+        if(!fs.existsSync(storeName)) {
+            console.log(`${chalk.green('Criando o diretorio da store!')}`);
+            fs.mkdirSync(`${__dirname}/${storeName}`);
+        }
+
+        for(let i in FILES) {
+            await createFile(storeName, FILES[i]);
+        }
+        console.log(`${chalk.green('Processo realizado com sucesso!')}`);
+    });
+
+program.parse(process.argv);
